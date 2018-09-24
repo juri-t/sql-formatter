5a7c2157e644157eb64656cd3c457299f0c9e040
Juri.Takahashi <puyo24.juri.takahashi@gmail.com>
2018-09-25 01:29:55 +0900

    feat 予約語は大文字に変換する

--- bin/sqlformatter.rb
+++ bin/sqlformatter.rb
@@ -2,8 +2,19 @@
 =begin
 	SQLをフォーマットします
 =end
-lines = readlines
+RESERVED_WORDS = ['select', 'from', 'where', 'join', 'and', 'or', 'create', 'table', 'as',
+	'inner', 'left', 'right', 'full', 'cross', 'union', 'all', 'in']
 
+lines = readlines
 for line in lines
-	print line
+	for word in line.split()
+		if RESERVED_WORDS.include?(word.downcase) then
+			print word.upcase
+		else
+			print word
+		end
+
+		print " "
+	end
 end
+print "\n"
