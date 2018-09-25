d272e243b2e7f2f6cc9dabbbb2adde6233c0fb86
Juri.Takahashi <puyo24.juri.takahashi@gmail.com>
2018-09-25 22:05:42 +0900

    feat ジョイン句対応

--- bin/sqlformatter.rb
+++ bin/sqlformatter.rb
@@ -2,7 +2,9 @@
 =begin
 	SQLをフォーマットします
 =end
-NEWLINE_WORDS = ['select', 'from', 'where', 'and', 'or', 'create', 'inner', 'left', 'right', 'full', 'cross', 'union', 'group', 'order']
+NEWLINE_WORDS = ['select', 'from', 'where', 'and', 'or', 'create', 'inner', 'left', 'right', 'full', 'cross', 'union', 'group', 'order', 'on']
+INDENT_WORDS = ['inner', 'left', 'right', 'full', 'cross']
+INDENT_END_WORDS = ['where', 'group', 'order']
 
 
 RESERVED_WORDS = []
@@ -12,16 +14,26 @@ File.open("config/reserved_words.txt", "r"){|f|
 	}
 }
 
+def indent(indent_rank)
+	Array.new(indent_rank * 6, " ").join()
+end
 
 lines = readlines
 is_newline = false
+should_indent = false
 for line in lines
 	for word in line.gsub(/\s?,\s?/, ", ").gsub(/\s*=\s*/, " = ").split()
+		if INDENT_WORDS.include?(word.downcase) then
+			should_indent = true
+		elsif INDENT_END_WORDS.include?(word.downcase) then
+			should_indent = false
+		end
+
 		# 改行させる?
 		if NEWLINE_WORDS.include?(word.downcase) then
-			print "\n" + Array.new(6 - word.length, " ").join()
+			print "\n" + Array.new(6 - word.length, " ").join() + indent(should_indent ? 1 : 0)
 		elsif is_newline then
-			print Array.new(7, " ").join()
+			print Array.new(7, " ").join() + indent(should_indent ? 1 : 0)
 		end
 
 		if RESERVED_WORDS.include?(word.downcase[/\w+/]) then
