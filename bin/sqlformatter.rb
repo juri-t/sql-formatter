9352eb62372bcf2efc6bd1499237deb454dd65e1
Juri.Takahashi <puyo24.juri.takahashi@gmail.com>
2018-09-25 09:28:50 +0900

    feat 改行とインデント揃え

--- bin/sqlformatter.rb
+++ bin/sqlformatter.rb
@@ -6,19 +6,41 @@ NEWLINE_WORDS = ['select', 'from', 'where', 'and', 'or', 'create', 'inner', 'lef
 
 RESERVED_WORDS = NEWLINE_WORDS.concat(['join', 'table', 'as', 'all', 'in'])
 
+def format(word)
+	if word.include?("=")
+		word.gsub("=", " = ")
+	else
+		word
+	end
+end
+
 
 lines = readlines
+is_newline = false
 for line in lines
 	for word in line.split()
-		if RESERVED_WORDS.include?(word.downcase) then
-			print "\n" + word.upcase
-		elsif NEWLINE_WORDS.include?(word.downcase) then
-			print "\n" + word
-		else
-			print word
+		# 改行させる?
+		if NEWLINE_WORDS.include?(word.downcase) then
+			print "\n" + Array.new(6 - word.length, " ").join()
+		elsif is_newline then
+			print Array.new(7, " ").join()
 		end
 
-		print " "
+		if RESERVED_WORDS.include?(word.downcase) then
+			print format(word.upcase)
+		else
+			print format(word)
+		end
+
+		# 改行させる?
+		if word.end_with?(',') then
+			print "\n"
+			is_newline = true
+		else
+			print " "
+			is_newline = false
+		end
+
 	end
 end
 print "\n"
